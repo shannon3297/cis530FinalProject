b79a371839f132157979eef59bfbadb232265eee
hiyori <hiyori@seas.upenn.edu>
2021-04-08 23:45:53 -0400

    i give up what the heck

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ def getKeysTokens(user):
     return [consumer_key, consumer_secret,access_token,access_token_secret]
 
 consumer_key, consumer_secret, access_token, access_token_secret = getKeysTokens('hiyori')
+pd.set_option("display.precision", 20)
 t = Twarc(consumer_key, consumer_secret, access_token, access_token_secret)
 # change file name to whatever csv you downloaded from IEEE: https://ieee-dataport.org/open-access/coronavirus-covid-19-tweets-dataset
 first_file_num = 1 # TODO Change to the first file you're doing 
@@ -41,7 +42,9 @@ for num in file_range:
         filename = 'corona_tweets_0' + str(num) + '.csv'
     else:
         filename = 'corona_tweets_' + str(num) +'.csv'
-    df = pd.read_csv(filename, header=None)
+    df = pd.read_csv(filename)
+    # df = df.drop(df.index[0])
+    # print(df.head())
     # df.columns = ['id', 'sentiment']
     total_tweets = df.shape[0]
     # df.sort_values(df.columns[0], inplace=True)
@@ -50,20 +53,21 @@ for num in file_range:
     while last_tweet < total_tweets - 1:
         batch_size = min(5, total_tweets - last_tweet) # TODO change 5 to 300 
         ids = df[last_tweet:last_tweet+batch_size].iloc[:, 0]
-        print(ids)
+        # print(ids)
         ids.to_csv(filename.split('.')[0] + '_ids.csv', index=False)
+        last_tweet = total_tweets
         time.sleep(10)
-        df_2 = pd.read_csv(filename.split('.')[0] + '_ids.csv', header=None)
-        print(df_2)
+        # df_2 = pd.read_csv(filename.split('.')[0] + '_ids.csv', header=None)
+        # print(df_2)
         start_time = datetime.now()
         for tweet in t.hydrate(open(filename.split('.')[0] + '_ids.csv')):
-            print(tweet)
+            # print(tweet)
             # tweet = t.hydrate(line.split(',')[0])
-        # for tweet in hydrated:
+#         # for tweet in hydrated:
             try:
                 print('in try')
                 curr_tweet = dict()
-                # print(tweet['entities'])
+                print(tweet['entities'])
                 curr_tweet['text'] = tweet['full_text']
                 # print(tweet['full_text'])
                 curr_tweet['id'] = tweet['id']
