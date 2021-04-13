4e4de6d2246ab599f4153c16bc28fd425b3d4258
shannon3297 <46979940+shannon3297@users.noreply.github.com>
2021-04-13 16:43:03 -0400

    changed input to a list of IDs

--- sentiment-join-extract.py
+++ sentiment-join-extract.py
@@ -2,17 +2,23 @@ import pandas as pd
 import numpy as np
 
 if __name__ == "__main__":
-    ids_correct_format = input('The IDS in csvs are of type Number with 0 decimal places? y/n: ')
-    if ids_correct_format == 'y':
-        # get filenames 
-        hydrated_file = input("Type the name of the hydrated file (ex: corona_tweets_01_hydrated.csv):")
-        sentiment_id_file = input("Type the name of the sentiment+id file (ex: corona_tweets_01.csv):")
+    # get file IDs
+    input_nums = input("Type a list of hydrated_sentiment/IEEE downloaded csv file IDs space-delimited, ex: 01 15 100 388:")
+    file_nums = map(int, input_nums.split())
+    # join df for all files inputted
+    for file_num in file_nums:
+        if file_num < 10:
+            hydrated_file = 'corona_tweets_0' + str(file_num)+ '_hydrated.csv'
+            sentiment_id_file = 'corona_tweets_0' + str(file_num) + '.csv'
+        else:
+            hydrated_file = 'corona_tweets_' + str(file_num) + '_hydrated.csv'
+            sentiment_id_file = 'corona_tweets_' + str(file_num) + '.csv'
 
-        #read files
+        # read files
         hydrated_df = pd.read_csv(hydrated_file, header=None)
         sentiment_id_df = pd.read_csv(sentiment_id_file, header=None)
 
-        # set column names 
+        # set column names
         hydrated_df.columns = ['text', 'id', 'place', 'created_at', 'user_location', 'user_name', 'followers_count', 'retweet_count', 'favorite_count', 'hashtags']
         sentiment_id_df.columns = ['id', 'sentiment']
 
@@ -22,15 +28,15 @@ if __name__ == "__main__":
         values = ['negative', 'neutral', 'positive']
         sentiment_id_df['sentiment'] = np.select(criteria, values, 'NA')
 
-        # join hydrated_df with sentiment_id_df        
+        # join hydrated_df with sentiment_id_df
         joined_df = hydrated_df.join(sentiment_id_df.set_index('id'), on='id', how='left')
 
-        # write to CSV 
-        joined_df.to_csv(hydrated_file.split(".")[0] + '_sentiments.csv', index=False)
-        print('Check out joined file at ' + hydrated_file.split(".")[0] + '_sentiments.csv')
-        joined_df['sentiment'].to_csv(sentiment_id_file.split(".")[0] + '_sentiments.csv', index=False)
-        print('Check out gold label sentiments at ' + sentiment_id_file.split(".")[0] + '_sentiments.csv')
-        joined_df.to_csv(sentiment_id_file.split(".")[0] + '_id_sentiments.csv', index=False, columns=['id', 'sentiment'])
-        print('Check out IDs and gold label sentiments at ' + sentiment_id_file.split(".")[0] + '_id_sentiments.csv')
-    else:
-        print('Made the IDS in csvs of type Number with 0 decimal places!')
+        # write to CSV
+        joined_file = hydrated_file.split(".")[0] + '_sentiments.csv'
+        joined_df.to_csv(joined_file, index=False)
+        print('Check out joined file at ', joined_file)
+    # folder got too crazy with so many outputs
+    # joined_df['sentiment'].to_csv(sentiment_id_file.split(".")[0] + '_sentiments.csv', index=False)
+    # print('Check out gold label sentiments at ' + sentiment_id_file.split(".")[0] + '_sentiments.csv')
+    # joined_df.to_csv(sentiment_id_file.split(".")[0] + '_id_sentiments.csv', index=False, columns=['id', 'sentiment'])
+    # print('Check out IDs and gold label sentiments at ' + sentiment_id_file.split(".")[0] + '_id_sentiments.csv')
