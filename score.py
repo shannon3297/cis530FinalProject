6ed9063240f85ecc3cf8498ccf30c8c173b99a48
hiyori <hiyori@seas.upenn.edu>
2021-04-12 21:06:30 -0400

    changed to f1 score

--- score.py
+++ score.py
@@ -1,32 +1,24 @@
 import csv
 from sklearn.metrics import *
 from itertools import chain
+import pandas as pd 
 
 if __name__ == "__main__":
-    gold = input("Type the .csv gold label file (ex: gold.csv):")
-    gold_file = gold
-    pred = input("Type the .csv prediction file (ex: pred.csv):")
-    pred_file = pred
-    with open(gold_file, newline='') as gf:
-        y_true = list(csv.reader(gf))
-        y_true = list(chain.from_iterable(y_true))
-        y_true = [float(i) for i in y_true]
-    with open(pred_file, newline='') as pf:
-        y_pred = list(csv.reader(pf))
-        y_pred = list(chain.from_iterable(y_pred))
-        y_pred = [float(i) for i in y_pred]
-    # TODO: what metric should we use?
+    # get file names 
+    gold_file = input("Type the .csv gold label file (ex: gold.csv):")
+    pred_file = input("Type the .csv prediction file (ex: pred.csv):")
+
+    # read csvs
+    y_true = pd.read_csv(gold_file)
+    y_pred = pd.read_csv(pred_file)
+    
+    # get and print score 
+    labels = ['negative', 'neutral', 'positive']
     try:
-        mean_absolute_error = mean_absolute_error(y_pred, y_true)
-        mean_squared_error = mean_squared_error(y_pred, y_true)
-        max_error = max_error(y_pred,y_true)
-        explained_variance_score=explained_variance_score(y_pred,y_true)
-        r2_score = r2_score(y_pred,y_true)
-        print("Mean Squared Error: ", mean_squared_error)
-        print("Mean Absolute Error: ", mean_absolute_error)
-        print("Max Error: ", max_error)
-        print("Explained Variance Score: ", explained_variance_score)
-        print("R2 Score: ", r2_score)
+        f1_score_classes = f1_score(y_true, y_pred, labels=labels, average=None)
+        f1_score_overall = f1_score(y_true, y_pred, labels=labels, average='weighted')
+        print("F1 score for each class: ", f1_score_classes)
+        print("F1 score overall: ", f1_score_overall)
     except Exception as e:
         print("Invalid files")
         print("Exception: ", e)
