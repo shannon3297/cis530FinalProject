a146958faed1da6d248b6df67dcc936b746f4464
hiyori <hiyori@seas.upenn.edu>
2021-04-18 12:50:10 -0400

    switched to swifter.apply, changed profanity check to alt-profanity-check 0.24.0

--- cleaning-features.py
+++ cleaning-features.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from better_profanity import profanity
+# from better_profanity import profanity
+from profanity_check import predict
 import emoji
 import re
+import swifter 
 
 def is_emoji(char):
     return char in emoji.UNICODE_EMOJI
@@ -17,9 +19,9 @@ if __name__ == "__main__":
     # data cleaning
     header = ['text', 'id', 'place', 'created_at', 'user_location', 'user_name', 'followers_count', 'retweet_count',
               'favorite_count', 'hashtags', 'sentiment']
-    # files = ['train.csv','dev.csv','test.csv']
+    files = ['train.csv','dev.csv','test.csv']
     directory = 'train-dev-test/'
-    files = ['train_mini.csv']
+    # files = ['train_mini.csv']
     for file in files:
         print('cleaning and extracting features from', file)
         df = pd.DataFrame(columns=header)
@@ -29,26 +31,26 @@ if __name__ == "__main__":
 
         # add other features 
         # presence of numbers 
-        df['num_present'] = df['text'].map(lambda x: any(map(str.isdigit, x)))
+        df['num_present'] = df['text'].swifter.apply(lambda x: any(map(str.isdigit, x)))
         # presence of "trump" 
-        df['trump_present'] = df['text'].map(lambda x: 'trump' in x.lower())
+        df['trump_present'] = df['text'].swifter.apply(lambda x: 'trump' in x.lower())
         # presence of hashtags 
-        df['hashtag_present'] = df['hashtags'].map(lambda x: isinstance(x, str))
+        df['hashtag_present'] = df['hashtags'].swifter.apply(lambda x: isinstance(x, str))
         # presence of covid/pandemic/quarantine-related words (from IEEE Dataport)
         covid_words = ["corona", "coronavirus", "covid", "covid19", "covid-19", "sarscov2", "sars cov2", "sars cov 2", "covid_19", "ncov", "ncov2019", "2019-ncov", "pandemic", "2019ncov", "quarantine", "lockdown", "social distancing", "strain", "strains", "variant", "variants"]
-        df['covid'] = df['text'].map(lambda x: any(word in x.lower() for word in covid_words))
+        df['covid'] = df['text'].swifter.apply(lambda x: any(word in x.lower() for word in covid_words))
         # presence of vaccine-related words 
         vaccine_words = ["vaccine", "vaccines", "corona vaccine", "corona vaccines", "#coronavaccine", "#coronavaccines", "vax", "pfizer", "biontech", "moderna"]
-        df['vaccine'] = df['text'].map(lambda x: any(word in x.lower() for word in vaccine_words))
+        df['vaccine'] = df['text'].swifter.apply(lambda x: any(word in x.lower() for word in vaccine_words))
         # text includes profanity or offensive language 
-        df['profanity_present'] = df['text'].map(lambda x: profanity.censor(x) != x)
+        df['profanity_present'] = df['text'].swifter.apply(lambda x: True if predict([x])[0] else False)
         # text includes emojis
-        df['emoji_present'] = df['text'].map(lambda x: any(map(is_emoji, x)))
+        df['emoji_present'] = df['text'].swifter.apply(lambda x: any(map(is_emoji, x)))
         # presence of links
-        df['url_present'] = df['text'].map(lambda x: True if contains_url(x) else False)
+        df['url_present'] = df['text'].swifter.apply(lambda x: True if contains_url(x) else False)
         # presence of "?", "!", "?!", "!?"
         question_exclamation_list = ["?", "!", "?!", "!?"]
-        df['question_exclamation_present'] = df['text'].map(lambda x: any(word in x for word in question_exclamation_list))
+        df['question_exclamation_present'] = df['text'].swifter.apply(lambda x: any(word in x for word in question_exclamation_list))
 
         # move sentiment label to last column: THIS SHOULD BE LAST LINE OF CODE BEFORE df.to_csv
         # reference: https://stackoverflow.com/questions/25122099/move-column-by-name-to-front-of-table-in-pandas
