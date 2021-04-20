5fdf562b3fc8c0e4a28dd12303d3348f9a4d9266
Varun Ramakrishnan <vramakrishnan1999@gmail.com>
2021-04-20 16:22:45 -0400

    added jupyter model

--- model.py
+++ model.py
@@ -1,7 +1,13 @@
 # This is the model file where we run the implemented log reg model
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
 
 # Load data
+train_feats = pd.read_csv('cleaned_feats/train_cleaned_features.csv')
+dev_feats = pd.read_csv('cleaned_feats/dev_cleaned_features.csv')
+test_feats = pd.read_csv('cleaned_feats/test_cleaned_features.csv')
+
+train_feats.head(5)
 
 # Clean data
 
