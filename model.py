e24158a298e8c6bbc7ce91b2c669807d6116e831
Varun Ramakrishnan <vramakrishnan1999@gmail.com>
2021-04-20 13:25:24 -0400

    more progress model

--- model.py
+++ model.py
@@ -1,1 +1,19 @@
 # This is the model file where we run the implemented log reg model
+from sklearn.linear_model import LogisticRegression
+
+# Load data
+
+# Clean data
+
+# Format data for model
+
+# LDA?
+
+# Instantiate model - Multinomial logistic regression
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+
+# Train model
+
+# Test model (use scores)
+
+# 
